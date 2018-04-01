493ce5904e81b2ceb36826084ea2c774ea8cf225
Pablo Velez <Pablovelez@AL-COL-02313.local>
2018-03-31 23:13:46 -0500

    Add the multilevel menu html and css

--- js/multilevel.js
+++ js/multilevel.js
@@ -1,12 +1,12 @@
 var submenu = document.querySelector(".multilevel-nav ul.multilevel");
-submenu.parentElement.addEventListener("click", function(e) {
+submenu.parentElement.querySelector('a').addEventListener("click", function(e) {
     e.preventDefault();
     var class_list = submenu.classList;
     class_list.forEach(element => {
-        if (element == "hidden") {
-            submenu.classList.remove("hidden");
+        if (element == "toggle-on") {
+            submenu.classList.remove("toggle-on");
         } else {
-            submenu.classList.add("hidden");
+            submenu.classList.add("toggle-on");
         }
     });
 });
