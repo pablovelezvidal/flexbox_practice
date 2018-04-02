fe17798624d5686454a52a7dac2dba3af1d566ee
Pablo Velez <Pablovelez@AL-COL-02313.local>
2018-04-01 23:54:06 -0500

    practicing callbacks vs promises vs observables, part 1 - callbacks

--- js/multilevel.js
+++ js/multilevel.js
@@ -1,5 +1,5 @@
 var submenu = document.querySelector(".multilevel-nav ul.multilevel");
-submenu.parentElement.querySelector('a').addEventListener("click", function(e) {
+submenu.parentElement.querySelector('a').addEventListener("click", function (e) {
     e.preventDefault();
     var class_list = submenu.classList;
     class_list.forEach(element => {
@@ -10,3 +10,38 @@ submenu.parentElement.querySelector('a').addEventListener("click", function(e) {
         }
     });
 });
+
+
+/* Repasando Callbacks */
+(() => {
+    var textoH3 = document.querySelector("div.texto");
+    var boton = textoH3.parentElement.querySelector("#action");
+
+    const setTexto = texto => {
+        textoH3.textContent = texto;
+    }
+
+    const checkAuth = callback => {
+        setTexto("Verificando identidad...");
+        setTimeout(() => {
+            callback(true);
+        }, 2000);
+    };
+
+    const fetchName = callback => {
+        setTexto("Obteniendo el usuario...");
+        setTimeout(() => {
+            callback({nombre : "Pablo Andres Velez Vidal"});
+        }, 2000);
+    }
+
+    boton.addEventListener("click", () => {
+        checkAuth(auth => {
+            if (auth) {
+                fetchName(user => {
+                    setTexto(user.nombre);
+                }) 
+            }
+        });
+    });
+})();
