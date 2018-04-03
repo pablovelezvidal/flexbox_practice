49e75f79f3381328835d14cc615955884d4a8b34
Pablo Velez <Pablovelez@AL-COL-02313.local>
2018-04-02 22:49:28 -0500

    Remembering promises

--- js/multilevel.js
+++ js/multilevel.js
@@ -14,7 +14,7 @@ submenu.parentElement.querySelector('a').addEventListener("click", function (e) 
 
 /* Repasando Callbacks */
 (() => {
-    var textoH3 = document.querySelector("div.texto");
+    var textoH3 = document.querySelector("#async-callback div.texto");
     var boton = textoH3.parentElement.querySelector("#action");
 
     const setTexto = texto => {
@@ -45,3 +45,45 @@ submenu.parentElement.querySelector('a').addEventListener("click", function (e) 
         });
     });
 })();
+
+/* Repasando Promises */
+(() => {
+    var textoH3 = document.querySelector("#async-promises div.texto");
+    var boton = textoH3.parentElement.querySelector("#action");
+
+    const setTexto = texto => {
+        textoH3.textContent = texto;
+    }
+
+    const checkAuth = () => {
+        return new Promise((resolve, reject) => {
+            setTexto("Verificando identidad...");
+            setTimeout(() => {
+                resolve(true);
+            }, 2000);
+        })
+
+    };
+
+    const fetchName = () => {
+        return new Promise((resolve, reject) => {
+            setTexto("Obteniendo el usuario...");
+            setTimeout(() => {
+                resolve({nombre : "Pablo Andres Velez Vidal"});
+            }, 2000);
+        })
+    }
+
+    boton.addEventListener("click", () => {
+        checkAuth()
+        .then( auth => {
+            if (auth) {
+                return fetchName();
+            }
+        })
+        .then( user => {
+            setTexto(user.nombre);
+        });
+    });
+})();
+
